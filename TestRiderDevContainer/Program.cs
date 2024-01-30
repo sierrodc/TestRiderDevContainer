d275b626173e8e1a42cac6b37a7104f0e64b322d
Roberto.Sarati <Roberto.Sarati@PCLUGPOWRSAR>
2024-01-30 11:59:05 +0100

    include devcontainer to allow rider

--- TestRiderDevContainer/Program.cs
+++ TestRiderDevContainer/Program.cs
@@ -2,4 +2,8 @@
 
 Console.WriteLine("Hello, World!");
 
+Console.WriteLine("Hello, World444!");
 Console.WriteLine("Hello, World3!");
+
+Console.WriteLine("has mismatching sizes: start offsets size is 1, range markers size is 1!");
+
